86cd7f86929ca03528bacdce73a856286a4734b8
minyoungkim21 <44901665+minyoungkim21@users.noreply.github.com>
2023-10-27 14:26:12 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup 
+  
+setup( 
+    name='bayesdll', 
+    description='Bayesian Deep Learning Library', 
+    author='Minyoung Kim', 
+    author_email='mikim21@gmail.com', 
+    packages=['bayesdll'], 
+    package_dir={'bayesdll': 'src/bayesdll'},
+) 
